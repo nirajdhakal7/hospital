2737f377c2c5e06480f64be50b527db06d2e8883
netkor <43111478+netkor@users.noreply.github.com>
2020-02-11 18:32:51 +0545

    Update in Gallery

---
+++ resources/views/admin/gallery/index.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+@section('title','Slider')
+@section('css')
+@stop
+@section('content')
+	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<h1 class="h2">Image Carousel</h1>
+		<div class="btn-toolbar mb-2 mb-md-0">
+			<div class="btn-group mr-2">
+				<a href="{{ route('carousel.create')}}" class="btn btn-sm btn-outline-secondary">Add Carousel</a>
+				<button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+			</div>
+		</div>
+	</div>
+	@isset($carousels)
+		<div class="pb-2 mb-3 border-bottom">
+			<div class="card-deck">
+				@foreach($carousels as $card)
+					@include('components.card',['route'=>'carousel'])
+				@endforeach
+			</div>
+		</div>
+		<div class="container">
+			{{ $carousels->links() }}
+		</div>
+	@endisset
+@endsection
+
